b1dc87fe8bc7f3c1f9c992c0eecc4cc8393eaa33
Ruben Dario Jimenez <rubenarenas31@aragon.unam.mx>
2021-08-02 16:33:58 -0500

    Este es un for each
    en java

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+
+public class TestForEach {
+    public static void main(String[] args) {
+        int edades[] = {5,6,7,8,9};
+        for(int edad: edades){
+            System.out.println("edad = " + edad);
+        }
+    }
+}
